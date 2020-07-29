fc86366ced41647b228e7398744b0a947d853a92
bilinski <abc@example.com>
2020-07-29 14:45:18 +0200

    updating projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,6 +8,8 @@ import Ozdobychoinkowe from '../images/ozdobychoinkowe.png';
 import Englishlector4u from '../images/englishlectoru.png';
 import Marsell from '../images/marsellbrzesko.png';
 import Fastmask from '../images/fastmask.png';
+import Nowan from '../images/nowan.png';
+import Muzeumbombki from '../images/muzeumbombki.png';
 import { Paper } from '@material-ui/core';
 
 
@@ -38,18 +40,19 @@ class Project extends Component {
       <Container fluid style={center}>
         <Row>
           <Col fluid> 
-            <Image style={shadowandwidth} src={Cpbiuro} fluid/>
-            <Paper elevation={10} style={paper}>
-              <p className="text-center maintext">www.biurocp.pL<br></br>HTML/CSS<br></br>Projekt Logo, <br></br>Grafika Dedykowana. <br></br>Responsywna</p>
-            </Paper>
             <Image style={shadowandwidth} src={Englishlector4u} fluid/>
             <Paper elevation={10} style={paper}>
-              <p> www.englishlector4u.pl <br></br>System CMS WordpressPanel Klienta Panel Admina Projekt Logo Grafika Dedykowana<br></br>Sklep: Woocommerce<br></br>Responsywna</p>
+              <p> www.englishlector4u.pl <br></br>System CMS Wordpress, Panel Klienta, Panel Admina, Projekt Logo, Grafika Dedykowana<br></br>Sklep: Woocommerce<br></br>Responsywna</p>
             </Paper>
-            <Image style={shadowandwidth} src={Fastmask} fluid/>
+            <Image style={shadowandwidth} src={Nowan} fluid/>
             <Paper elevation={10} style={paper}>
-              <p> www.fastmask.pl <br></br>Sklep internetowy, System CMS Wordpress<br></br>Sklep: Woocommerce<br></br>Responsywna</p>
+              <p className="text-center maintext">www.nowanbhp.pl<br></br>Sklep internetowy, System CMS Wordpress<br></br>Sklep: Woocommerce<br></br>Responsywna<br></br>Projekt Logo,<br></br>Grafika Dedykowana. <br></br>Responsywna</p>
             </Paper>
+            <Image style={shadowandwidth} src={Muzeumbombki} fluid/>
+            <Paper elevation={10} style={paper}>
+              <p className="text-center maintext">www.muzeumbombki.pl<br></br>HTML/CSS/JavaScript<br></br>Projekt Logo<br></br>Grafika Dedykowana<br></br>Responsywna</p>
+            </Paper>
+ 
           </Col>
           <Col>  
             <Image style={shadowandwidth} src={Ozdobychoinkowe} fluid/>
@@ -59,6 +62,14 @@ class Project extends Component {
             <Image style={shadowandwidth} src={Marsell} fluid/>
             <Paper elevation={10} style={paper}>
             <p className="text-center">www.marsellbrzesko.pl <br></br>HTML/CSS<br></br>Grafika Dedykowana<br></br>jQuery<br></br>Responsywna</p>
+            </Paper>
+            <Image style={shadowandwidth} src={Cpbiuro} fluid/>
+            <Paper elevation={10} style={paper}>
+              <p className="text-center maintext">www.biurocp.pL<br></br>HTML/CSS<br></br>Projekt Logo, <br></br>Grafika Dedykowana. <br></br>Responsywna</p>
+            </Paper>
+            <Image style={shadowandwidth} src={Fastmask} fluid/>
+            <Paper elevation={10} style={paper}>
+              <p> www.fastmask.pl <br></br>Sklep internetowy, System CMS Wordpress<br></br>Sklep: Woocommerce<br></br>Responsywna</p>
             </Paper>
           </Col>
         </Row>
